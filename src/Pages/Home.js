041f8726f70fdf1bbee089f4f9498fa29624d597
Sai Uttarkar <usai7261@gmail.com>
2024-03-13 10:53:38 +0530

    ui changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,60 +1,125 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
+	FaDatabase,
 	FaGithub,
 	FaInstagram,
+	FaLaptopCode,
 	FaLinkedinIn,
 	FaPaperclip,
 } from "react-icons/fa";
+import { TbCloudCode } from "react-icons/tb";
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
+import { MdConnectWithoutContact, MdOpenInNew } from "react-icons/md";
 
 import Nav from "../Components/Nav";
 import "../Home.css";
+import socialmedia from "../Assets/socialmedia.webp";
+import exam from "../Assets/onlinexam.png";
+import fileshare from "../Assets/fileshare.png";
+import api from "../Assets/api.jpeg";
+import auth from "../Assets/auth.png";
+import compvision from "../Assets/compvision.jpg";
+import auction from "../Assets/auction.jpg";
 
 export default function Home() {
 	const skills = [
-		{ name: "PHP", rating: "4.9/5" },
-		{ name: "Node.js", rating: "4.5/5" },
-		{ name: "Express.js", rating: "4.5/5" },
-		{ name: "React JS", rating: "4.6/5" },
-		{ name: "Python", rating: "4.7/5" },
-		{ name: "Firebase", rating: "3.8/5" },
-		{ name: "HTML", rating: "4.9/5" },
-		{ name: "Tailwindcss", rating: "4.9/5" },
-		{ name: "MySQL", rating: "4.5/5" },
-		{ name: "MongoDB", rating: "4/5" },
-		{ name: "Rest API", rating: "4.9/5" },
-		{ name: "MongoDB", rating: "4/5" },
+		{
+			category: "Frontend",
+			list: ["HTML&CSS", "React JS", "Tailwind", "Bootstrap"],
+		},
+		{
+			category: "Backend",
+			list: [
+				"Node.js",
+				"Express.js",
+				"PHP",
+				"Python",
+				"Firebase",
+				"RESTful APIs",
+			],
+		},
+		{
+			category: "Databases",
+			list: ["MongoDB", "MySQL"],
+		},
+		{
+			category: "Others",
+			list: ["C/C++", "Communication"],
+		},
 	];
+
 	const projects = [
 		{
-			name: "Auction Platform",
-			link: "https://github.com/usai729/mern-auction-assignment",
-			languages: ["MERN Stack"],
-		},
-		{
-			name: "Online Examination System",
-			link: "https://github.com/usai729/Online-Exam-System",
-			languages: ["PHP", "HTML", "CSS", "JavaScript", "MySQL"],
-		},
-		{
-			name: "File Sharing System",
+			title: "File Sharing System",
+			image: fileshare,
+			points: [
+				"Cloud storage of files",
+				"Separate admin and student pages",
+				"Separate logins",
+			],
 			link: "https://github.com/usai729/usai729-File_Sharing_And_Student_Management---PHP",
-			languages: ["PHP", "HTML", "CSS", "JavaScript", "MySQL"],
 		},
 		{
-			name: "Social Media Backend API",
+			title: "Social Media Backend API",
+			image: api,
+			points: [
+				"Authentication Using JWT",
+				"Upload, edit, delete posts",
+				"Edit and create profiles",
+				"Follow, unfollow, like, unlike",
+			],
 			link: "https://github.com/usai729/nodejs-socialmedia-backend-",
-			languages: ["Node.js", "MongoDB"],
 		},
 		{
-			name: "Car Speed Detection",
-			link: "https://github.com/usai729/Car-Speed-Detection-OpenCV-Python",
-			languages: ["Python"],
+			title: "Online Examination Platform",
+			image: exam,
+			points: [
+				"Login for admin and student",
+				"Attend exam, automatic result evaluation and answer key generation",
+				"Schedule and edit examinations",
+				"Student registration when allowed by admin",
+			],
+			link: "https://github.com/usai729/Online-Exam-System",
 		},
-		{ name: "Social Media Frontend", link: null, languages: ["React JS"] },
+		{
+			title: "Online Auction Platform",
+			image: auction,
+			points: [
+				"Login for seller and buyer",
+				"Posting new products for auction",
+				"Implemented payment gateway",
+			],
+			link: "https://github.com/usai729/mern-auction-assignment",
+		},
+		{
+			title: "Car Speed Detection",
+			image: compvision,
+			points: [],
+			link: "https://github.com/usai729/Car-Speed-Detection-OpenCV-Python",
+		},
+		{
+			title: "Social Media Frontend",
+			image: socialmedia,
+			points: [
+				"Built using ReactJS",
+				"Idea was to combine the UI features of the popular social media applications",
+			],
+			link: null,
+		},
+		{
+			title: "Authentication System",
+			image: auth,
+			points: [
+				"JWT for login and signup",
+				"Forgot and reset passwords via email",
+				"Originally build as an assignment",
+			],
+			link: "https://github.com/usai729/atg",
+		},
 	];
 
 	return (
-		<div className="overflow-hidden mb-10">
+		<div className="overflow-hidden">
 			<Nav />
 			<div className="h-[70vh] p-10 flex justify-center items-center bg-gradient-to-tr from-gray-900 to-black">
 				<div className="flex items-center justify-center p-5 flex-col gap-2 max-w-[100vw] md:max-w-[15vw] text-white">
@@ -66,7 +131,8 @@ export default function Home() {
 						<span className="cursor">|</span>
 					</p>
 					<p className=" font-poppins w-full text-right">
-						A programming enthusiast from Hyderabad, India!
+						An avid Programmer based in the vibrant city of
+						Hyderabad, India!
 					</p>
 					<ul className="flex items-center justify-center mt-10 gap-7 list-none md:hidden w-full">
 						<li>
@@ -105,67 +171,162 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
-			<div className="p-1 bg-gradient-to-r from-purple-700 via-blue-400 to-purple-700 w-screen h-4"></div>
+			<Line />
+
 			<div className="mt-5 grid place-items-center">
-				<div className="skills mt-10">
+				<div className="skills mt-5 p-2">
 					<h3 className="text-gray-900 text-xl font-bold mb-5">
 						Skills
 					</h3>
-					<div className="list-none flex flex-wrap gap-3 max-w-[50vw]">
-						{skills.map((skill) => (
-							<Skill
-								name={skill.name}
-								rating={skill.rating}
+					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center">
+						{skills.map((skillCategory, index) => (
+							<SkillContainer
+								key={index}
+								category={skillCategory.category}
+								skills={skillCategory.list}
 							/>
 						))}
 					</div>
 				</div>
-				<div className="mt-5 border-1 border-gray-200 p-5">
+			</div>
+			<div className="mt-5 grid place-items-center">
+				<div className="skills mt-5 p-2">
 					<h3 className="text-gray-900 text-xl font-bold mb-5">
 						Projects
 					</h3>
-					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full place-items-center gap-5">
-						{projects.map((project) => {
+					<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 place-items-center">
+						{projects.map((project, index) => {
 							return (
 								<Project
-									name={project.name}
+									key={index}
+									title={project.title}
+									image={project.image}
 									link={project.link}
-									languages={project.languages}
+									points={project.points}
 								/>
 							);
 						})}
 					</div>
 				</div>
 			</div>
-            <p className="text-xs w-full text-center">The site isn't responsive just yet</p>
+			<div className="p-10 flex flex-col gap-4 justify-center h-[70vh] items-center bg-gradient-to-tr from-gray-900 to-black text-white mt-5">
+				<div className="flex flex-col md:flex-row gap-2 w-screen md:w-[40vw] p-10 md:p-0">
+					<div>
+						<p className="text-lg font-semibold font-poppins text-beech">
+							About me...
+						</p>
+						<p className="text-md leading-6 font-roboto text-beech">
+							Full-stack developer with a product vision, weaving
+							user experiences with MERN magic. Mastering the web
+							(HTML, CSS, JS, React, PHP) while exploring
+							marketing strategies to fuel engagement. Keen eye
+							for detail, analytical mind, and collaborative
+							spirit bridge the gap between tech and business.
+							Seeking opportunities to build impactful products
+							and collaborate with awesome teams!
+						</p>
+					</div>
+					{/* <div
+						className="github-card"
+						data-github="usai729"
+						data-width="400"
+						data-height=""
+						data-theme="default"
+					></div> */}
+				</div>
+				<div className="flex flex-col w-full md:flex-row justify-center items-center gap-2">
+					<button
+						className="flex items-center justify-center p-4 bg-c1 rounded-md gap-2 text-beech cursor-not-allowed w-full md:w-max"
+						disabled
+					>
+						<MdConnectWithoutContact size={25} />
+						Connect With Me
+					</button>
+					<a
+						href="https://github.com/usai729/uttarkar-sai"
+						target="_blank"
+						className="flex items-center justify-center p-4 text-c1 rounded-md gap-2 bg-white cursor-pointer w-full md:w-max"
+						disabled
+					>
+						<FaGithub size={25} />
+						Code to{" "}
+						<span className=" text-blue-900">
+							this<span className="text-purple-400">.site</span>
+						</span>
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-const Skill = ({ name, rating }) => (
-	<div className="skill flex items-center gap-2 mb-2 bg-gradient-to-tr from-gray-900 to-black rounded-md p-5 shadow-md drop-shadow-md">
-		<p className="text-gray-300 font-semibold">{name}</p>
-		<p className="text-gray-500">{rating}</p>
+const SkillContainer = ({ category, skills }) => (
+	<div className="flex flex-col gap-1 bg-c1 p-3 rounded-md m-2 md:m-0 w-[98vw] md:w-[27vw] lg:w-[17vw] h-[30vh] shadow-md font-poppins">
+		<h4 className="text-md font-semibold text-beech flex items-center gap-1">
+			{category.toLowerCase() === "databases" ? (
+				<FaDatabase />
+			) : category.toLowerCase() === "frontend" ? (
+				<FaLaptopCode />
+			) : category.toLowerCase() === "backend" ? (
+				<TbCloudCode />
+			) : (
+				<GiPerspectiveDiceSixFacesRandom />
+			)}
+			{category}
+		</h4>
+		<div className="flex flex-wrap gap-3 text-phblue text-xs p-2">
+			{skills.map((skill, index) => (
+				<p
+					key={index}
+					className={`text-gray-300 p-1 border-1 shadow-sm ${
+						skill.toLowerCase() === "node.js" ||
+						skill.toLowerCase() === "express.js" ||
+						skill.toLowerCase() === "react js" ||
+						skill.toLowerCase() === "tailwind" ||
+						skill.toLowerCase() === "restful apis"
+							? "border-[#ECA72C] shadow-[#ECA72C]"
+							: "border-[#BA7BA1]"
+					} rounded-md`}
+				>
+					{skill}
+				</p>
+			))}
+		</div>
 	</div>
 );
 
-const Project = ({ name, link, languages }) => (
-	<div className="p-3 flex flex-col gap-1 border-gray-200 border-1p5 w-[98vw] md:w-[18vw] min-h-[20vh] rounded-sm">
-		<p className="text-md font-semibold">{name}</p>
-		<div className="flex flex-wrap gap-2">
-			{languages?.map((language) => {
-				return (
-					<p className="p-1 rounded-md border-1 border-gray-300 w-max text-sm">
-						{language}
-					</p>
-				);
-			})}
+const Project = ({ title, link, points, image }) => (
+	<div className="flex flex-col justify-between gap-1 rounded-md w-[98vw] md:w-[27vw] xl:w-[17vw] min-h-[55vh] font-poppins border-1 border-gray-200 overflow-hidden">
+		<div>
+			<img
+				src={image}
+				alt=""
+				className="rounded-t-md h-[20vh] w-full object-cover object-center"
+			/>
+			<div className="flex flex-col justify-center p-2 gap-2">
+				<p className="text-md font-semibold">{title}</p>
+				<ol className="list-disc text-sm font-sans pl-4">
+					{points?.map((point, index) => (
+						<li key={index}>{point}</li>
+					))}
+				</ol>
+			</div>
 		</div>
-		<a
-			href={link}
-			className="text-xs text-blue-500"
-		>
-			Visit Repo
-		</a>
+		{link && (
+			<a
+				href={link}
+				target="_blank"
+				className="flex items-center gap-1 m-3 text-xs text-blue-500"
+			>
+				<MdOpenInNew /> View Code
+			</a>
+		)}
 	</div>
 );
+
+const Line = () => {
+	return (
+		<div className="p-1 bg-gradient-to-r from-purple-700 via-blue-400 to-purple-700 w-screen h-4"></div>
+	);
+};
+// 14131A
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      borderWidth: {
-        "1p5": "1.5px",
-        1:"1px"
-      },
-      fontFamily: {
-        "micro5": '"Micro 5", sans-serif',
-        "press-start": '"Press Start 2P", system-ui',
-        "poppins": '"Poppins", sans-serif'
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			borderWidth: {
+				"1p5": "1.5px",
+				1: "1px",
+			},
+			fontFamily: {
+				micro5: '"Micro 5", sans-serif',
+				"press-start": '"Press Start 2P", system-ui',
+				poppins: '"Poppins", sans-serif',
+				roboto: '"Roboto", sans-serif',
+			},
+			colors: {
+				c1: "#14131A",
+				beech: "#F7EBEC",
+				phblue: "#8AC6D0",
+			},
+		},
+	},
+	plugins: [],
+};
